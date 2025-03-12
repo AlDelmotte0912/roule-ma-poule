1c0053cdd3424e9f37627805f81b1bc14ff704c5
Alexandre Delmotte <delmotte.al@gmail.com>
2025-03-12 14:56:51 +0100

    flex toujours en css je vais passer a scss

--- web/app/themes/roule-ma-poule/resources/views/sections/header.blade.php
+++ web/app/themes/roule-ma-poule/resources/views/sections/header.blade.php
@@ -1,11 +1,13 @@
-<header class="banner">
+<header class="banner flex flex justify-between">
   <a class="brand" href="{{ home_url('/') }}">
-    {!! $siteName !!}
+    <img class="w-32" src="{{ Vite::asset('resources/images/logo-rmp.svg') }}" alt="Logo roule ma poule">
   </a>
-<p class="text-primary font-display">je suis un monstre</p>
+
   @if (has_nav_menu('primary_navigation'))
-    <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
+    <nav class="nav-primary bg-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
       {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
     </nav>
   @endif
+
+  <p class="text-primary bg-secondary">fgrnnnnnnnn</p>
 </header>
