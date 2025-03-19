f8d0f83e0e25d5cab02c2aeb83280e9494f6fb45
Alexandre Delmotte <delmotte.al@gmail.com>
2025-03-19 20:07:51 +0100

    casi finalisation du header, début page home

---
+++ web/app/themes/roule-ma-poule/resources/views/components/button.blade.php
@@ -0,0 +1,3 @@
+<a {{ $attributes->merge(['class' => 'bg-primary text-secondary text-sm inline-flex self-center py-3 px-7 rounded-full']) }}>
+  {{ $slot }}
+</a>
--- web/app/themes/roule-ma-poule/resources/views/sections/header.blade.php
+++ web/app/themes/roule-ma-poule/resources/views/sections/header.blade.php
@@ -1,13 +1,16 @@
-<header class="banner flex flex justify-between">
-  <a class="brand" href="{{ home_url('/') }}">
-    <img class="w-32" src="{{ Vite::asset('resources/images/logo-rmp.svg') }}" alt="Logo roule ma poule">
+<header class="banner bg-white shadow">
+  <div class="flex justify-end max-w-7xl mx-auto px-6 h-24">
+  <a class="brand mr-auto" href="{{ home_url('/') }}">
+    <img class="h-24 object-contain" src="{{ Vite::asset('resources/images/logo-rmp.svg') }}" alt="Logo roule ma poule">
   </a>
 
   @if (has_nav_menu('primary_navigation'))
-    <nav class="nav-primary bg-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
+    <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
       {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
     </nav>
   @endif
-
-  <p class="text-primary bg-secondary">fgrnnnnnnnn</p>
+    <x-button>
+      Boutton one
+    </x-button>
+  </div>
 </header>
